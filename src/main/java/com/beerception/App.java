b16af24d165dbbc16459f766d9bff49fb362a7f5
Milos <Milos@DESKTOP-LC1AG6G>
2017-10-20 19:43:52 +0200

    first commit

---
+++ src/main/java/com/beerception/App.java
@@ -0,0 +1,18 @@
+package com.beerception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Added plain spring boot class for running application.
+ * 
+ * @author Beerception team
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
